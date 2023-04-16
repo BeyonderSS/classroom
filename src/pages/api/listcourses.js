f7d442c9eea06e3b9e3efa3213f4d676f71e68f5
BeyonderSS <ssbeyonder@gmail.com>
2023-04-16 19:07:47 +0530

    cousrse-list-on-console

---
+++ src/pages/api/listcourses.js
@@ -0,0 +1,29 @@
+// api/listcourses.js 
+
+export async function getCoursesWithAccessToken(accessToken) {
+    const url = "https://classroom.googleapis.com/v1/courses";
+    const headers = {
+      Authorization: `Bearer ${accessToken}`
+    };
+  
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: headers
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        const courses = data.courses;
+        // You can process the courses data as needed
+        console.log(courses);
+        return courses;
+      } else {
+        throw new Error("Failed to fetch courses from Google Classroom API");
+      }
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+      throw error;
+    }
+  }
+  
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,104 +1,67 @@
-import React, { useState } from 'react';
-import SignInButton from '../components/SignInButton';
-import CoursesList from '../components/CoursesList';
-import { GoogleAuthProvider,getAuth, signInWithPopup, signOut } from "firebase/auth";
-import { auth } from '@/lib/firebase';
+import React, { useState } from "react";
+import SignInButton from "../components/SignInButton";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { getCoursesWithAccessToken } from "./api/listcourses";
 
 async function signInWithGoogle() {
-  // ... (same as the previous implementation)
-  
   const provider = new GoogleAuthProvider();
-  provider.addScope("https://www.googleapis.com/auth/classroom.courses.readonly");
+  provider.addScope(
+    "https://www.googleapis.com/auth/classroom.courses.readonly"
+  );
 
-  // const result =  getAuth();
-  // signInWithPopup(provider);
-  // const idToken = await result.user.getIdToken();
-let idToken;
-  const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
+  try {
+    const result = await signInWithPopup(auth, provider);
     const credential = GoogleAuthProvider.credentialFromResult(result);
-    idToken = credential.accessToken;
-    // The signed-in user info.
+    const accessToken = credential.accessToken;
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-   console.log(user)
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-  console.log(idToken)
-
-  const response = await fetch("/api/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ idToken, code: result.credential.accessToken }),
-  });
-
-}
-
-async function getCourses() {
-  // ... (same as the previous implementation)
-
-  async function getCourses() {
-    const response = await fetch("/api/courses");
-    const data = await response.json();
-  
-    if (response.ok) {
-      // Display the list of courses
-      console.log(data.courses);
-    } }                   
-  
+    console.log(user);
+    console.log("Access Token:", accessToken);
+    document.cookie = `accessToken=${accessToken}; path=/;`;
+    getCoursesWithAccessToken(accessToken);
+    // ... (rest of your code)
+  } catch (error) {
+    // Handle Errors here
+    console.error("Error signing in with Google:", error);
+  }
 }
 
 export default function Home() {
-  const [courses, setCourses] = useState([]);
   const [user, setUser] = useState(null);
 
   const handleSignIn = async () => {
     await signInWithGoogle();
-    const newCourses = await getCourses();
-    setCourses(newCourses);
+    // Call your API endpoint or perform any other tasks
   };
-
 
   async function handleSignOut() {
     try {
       // Sign out the user
       await signOut(auth);
-  console.log(auth)
+      console.log(auth);
       // Update the user state to null
       setUser(null);
-  
+
       // Clear the courses state
-      setCourses([]);
-  
+
       // You can also perform additional tasks, such as navigating to a different page or showing a success message
       console.log("Successfully signed out!");
     } catch (error) {
       console.error("Error signing out:", error);
     }
   }
-  
-  
 
   return (
     <div>
       <SignInButton onClick={handleSignIn} />
       <div>
-        <button onClick={handleSignOut}>
-Logout
-        </button>
+        <button onClick={handleSignOut}>Logout</button>
       </div>
-      <CoursesList courses={courses} />
     </div>
   );
 }
