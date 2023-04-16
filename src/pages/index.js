a162a4271b6f1d5f3904b7efe322a4c9d6a9181c
spideyO0 <dshantanu2003@gmail.com>
2023-04-16 19:21:15 +0530

    main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import SignInButton from "../components/SignInButton";
-import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-import { auth } from "@/lib/firebase";
-import { getCoursesWithAccessToken } from "./api/listcourses";
-
-async function signInWithGoogle() {
-  const provider = new GoogleAuthProvider();
-  provider.addScope(
-    "https://www.googleapis.com/auth/classroom.courses.readonly"
-  );
-
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-    const user = result.user;
-    console.log(user);
-    console.log("Access Token:", accessToken);
-    document.cookie = `accessToken=${accessToken}; path=/;`;
-    getCoursesWithAccessToken(accessToken);
-    // ... (rest of your code)
-  } catch (error) {
-    // Handle Errors here
-    console.error("Error signing in with Google:", error);
-  }
-}
+import AuthContext from "../contexts/AuthContext";
 
 export default function Home() {
-  const [user, setUser] = useState(null);
-
-  const handleSignIn = async () => {
-    await signInWithGoogle();
-    // Call your API endpoint or perform any other tasks
-  };
-
-  async function handleSignOut() {
-    try {
-      // Sign out the user
-      await signOut(auth);
-      console.log(auth);
-      // Update the user state to null
-      setUser(null);
-
-      // Clear the courses state
-
-      // You can also perform additional tasks, such as navigating to a different page or showing a success message
-      console.log("Successfully signed out!");
-    } catch (error) {
-      console.error("Error signing out:", error);
-    }
-  }
+  const { user, signInWithGoogle, signOut } = useContext(AuthContext);
 
   return (
     <div>
-      <SignInButton onClick={handleSignIn} />
-      <div>
-        <button onClick={handleSignOut}>Logout</button>
-      </div>
+      {/* Render your UI based on the user state */}
+      {user ? (
+        <button onClick={signOut}>Sign Out</button>
+      ) : (
+        <SignInButton onClick={signInWithGoogle} />
+      )}
     </div>
   );
 }
